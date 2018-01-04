0e4c2ea74ce20f155af92791b930c4f8d5137881
Paul Nittmann <pn@evait.de>
2018-01-04 11:58:23 +0100

    remove symlink

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -2,16 +2,6 @@
 set -e
 
 if [ "x$1" = 'x/usr/vpnserver/vpnserver' ]; then
-
-    # Linking Logs
-    for d in server_log security_log packet_log;
-    do
-        if [ ! -L /usr/vpnserver/$d ]; then
-          mkdir -p /var/log/vpnserver/$d
-          ln -s /var/log/vpnserver/$d /usr/vpnserver/$d
-        fi
-    done
-
     # Allow app to use ports < 1024 without root
     chown -R softether:softether /usr/vpnserver
     setcap 'cap_net_bind_service=+ep' /usr/vpnserver/vpnserver
